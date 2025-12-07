a1ace101cb29a701b7ced2c806bad0c293cde111
Leonardo Corti <leonardo.filippo@ymail.com>
2025-12-07 23:25:16 +0100

    quicker tests for day00

--- src/days/day00.rs
+++ src/days/day00.rs
@@ -2,13 +2,13 @@ use log::debug;
 
 pub fn part1(input: &str) -> String {
     debug!("debug in part1");
-    std::thread::sleep(std::time::Duration::from_micros(5));
+    // std::thread::sleep(std::time::Duration::from_micros(5));
     input.lines().count().to_string()
 }
 
 pub fn part2(input: &str) -> String {
     debug!("debug in part2");
-    std::thread::sleep(std::time::Duration::from_secs(1));
+    // std::thread::sleep(std::time::Duration::from_secs(1));
     input.len().to_string()
 }
 
